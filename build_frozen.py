4e763423e281accf70a1a90e401d2537e7242120
KmolYuan <pyslvs@gmail.com>
2019-11-07 15:10:01 +0800

    Build static inplace.

--- build_frozen.py
+++ build_frozen.py
@@ -4,5 +4,7 @@ from flask_frozen import Freezer
 from app import app
 
 if __name__ == '__main__':
-    app.config['FREEZER_DESTINATION'] = 'docs'
+    app.config['FREEZER_DESTINATION'] = '.'
+    app.config['FREEZER_REMOVE_EXTRA_FILES'] = False
+    app.config['FREEZER_RELATIVE_URLS'] = True
     Freezer(app).freeze()
