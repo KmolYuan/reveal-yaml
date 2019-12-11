a225707caea801d9037d139fec51d0b48605cfdf
KmolYuan <pyslvs@gmail.com>
2019-12-11 15:42:32 +0800

    Build in folder instead of root.

--- build_frozen.py
+++ build_frozen.py
@@ -4,7 +4,6 @@ from flask_frozen import Freezer
 from app import app
 
 if __name__ == '__main__':
-    app.config['FREEZER_DESTINATION'] = '.'
-    app.config['FREEZER_REMOVE_EXTRA_FILES'] = False
+    app.config['FREEZER_DESTINATION'] = 'build'
     app.config['FREEZER_RELATIVE_URLS'] = True
     Freezer(app).freeze()
