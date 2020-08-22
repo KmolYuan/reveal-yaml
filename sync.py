53dd0f8b488065503f3b802f0cc5472105fa612b
KmolYuan <pyslvs@gmail.com>
2020-08-22 11:18:02 +0800

    Use CDN instead of submodule.

--- sync.py
+++ sync.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from os.path import isdir
-from distutils.file_util import copy_file
 from distutils.dir_util import copy_tree, mkpath
+from urllib.request import urlretrieve
 import yaml
 import json
+
+
+def cdn(url: str, to: str) -> None:
+    """Download from CDNjs."""
+    to += url.rsplit('/', maxsplit=1)[-1]
+    urlretrieve("https://cdnjs.cloudflare.com/ajax/libs/" + url, to)
 
 
 def main():
@@ -15,14 +21,17 @@ def main():
             copy_tree("reveal.js/dist", f"reveal_yaml/static/{path}")
         else:
             copy_tree(f"reveal.js/{path}", f"reveal_yaml/static/{path}")
-    mkpath("reveal_yaml/static/js")
-    copy_file("jquery/dist/jquery.min.js", "reveal_yaml/static/js")
-    mkpath("reveal_yaml/static/ace")
-    copy_file("ace/src-min-noconflict/ace.js", "reveal_yaml/static/ace")
-    copy_file("ace/src-min-noconflict/mode-yaml.js",
-              "reveal_yaml/static/ace")
-    copy_file("ace/src-min-noconflict/theme-chrome.js",
-              "reveal_yaml/static/ace")
+    path = "reveal_yaml/static/js/"
+    mkpath(path)
+    cdn("jquery/3.5.1/jquery.min.js", path)
+    path = "reveal_yaml/static/ace/"
+    mkpath(path)
+    cdn("ace/1.4.12/ace.min.js", path)
+    cdn("ace/1.4.12/ext-searchbox.min.js", path)
+    cdn("ace/1.4.12/mode-yaml.min.js", path)
+    cdn("ace/1.4.12/theme-chrome.min.js", path)
+    cdn("ace/1.4.12/theme-monokai.min.js", path)
+    cdn("js-cookie/2.2.1/js.cookie.min.js", path)
     # Generate JSON schema
     with open("reveal_yaml/schema.yaml", 'r') as f:
         data = yaml.safe_load(f)
