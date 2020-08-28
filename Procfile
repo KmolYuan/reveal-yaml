09dd6eaddf2dc16f39146eab778d2d3c5afff8b9
KmolYuan <pyslvs@gmail.com>
2020-08-28 11:53:58 +0000

    Deploying to gh-pages from  @ f13f8c38a0f89724e8c7013c2ddabb17edb4983c 🚀

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: python sync.py && gunicorn reveal_yaml.editor:app
--- reveal_yaml/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__version__ = '2.0.3'
-__author__ = "Yuan Chang"
-__copyright__ = "Copyright (C) 2019-2020"
-__license__ = "MIT"
-__email__ = "pyslvs@gmail.com"
