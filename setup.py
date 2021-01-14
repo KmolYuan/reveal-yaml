35f4fddd6cf53cecbc25e2e70aed11846bae5f7c
KmolYuan <pyslvs@gmail.com>
2021-01-14 15:28:52 +0800

    Apply PEP517.

--- setup.py
+++ setup.py
@@ -1,27 +1,9 @@
 # -*- coding: utf-8 -*-
 
-__author__ = "Yuan Chang"
-__copyright__ = "Copyright (C) 2019-2020"
-__license__ = "MIT"
-__email__ = "pyslvs@gmail.com"
-
 from typing import Iterator
-from re import MULTILINE, search
 from os import walk
 from os.path import join
-from setuptools import setup, find_packages
-
-
-def read(path: str):
-    with open(path, 'r', encoding='utf-8') as f:
-        return f.read()
-
-
-def find_version(path: str):
-    m = search(r"^__version__ = ['\"]([^'\"]*)['\"]", read(path), MULTILINE)
-    if m:
-        return m.group(1)
-    raise RuntimeError("Unable to find version string.")
+from setuptools import setup
 
 
 def package_files(path: str) -> Iterator[str]:
@@ -35,32 +17,5 @@ extra_files = list(package_files(join('reveal_yaml', '.github')))
 extra_files.extend(package_files(join('reveal_yaml', 'static')))
 extra_files.extend(package_files(join('reveal_yaml', 'templates')))
 
-setup(
-    name='reveal_yaml',
-    version=find_version(join('reveal_yaml', '__init__.py')),
-    author=__author__,
-    author_email=__email__,
-    license=__license__,
-    description="A YAML, Markdown, reveal.js based Flask application. "
-                "Supports gh-pages deployment actions.",
-    long_description=read("README.md"),
-    long_description_content_type='text/markdown',
-    url="https://kmolyuan.github.io/reveal-yaml",
-    packages=find_packages(),
-    package_data={'reveal_yaml': ['py.typed', '*.yaml', 'schema.*'],
-                  '': extra_files},
-    entry_points={'console_scripts': ['rym=reveal_yaml.__main__:main']},
-    zip_safe=False,
-    python_requires=">=3.7",
-    install_requires=read('requirements.txt').splitlines(),
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Typing :: Typed",
-    ]
-)
+setup(package_data={'reveal_yaml': ['py.typed', '*.yaml', 'schema.*'],
+                    '': extra_files})
